01b62568f1d2f80441214fd9f6de51787c9aaffe
David Ilie <david@davidilie.com>
2021-06-16 12:18:40 +0200

    added sub-text

--- frontend/src/modules/landing/header/header.jsx
+++ frontend/src/modules/landing/header/header.jsx
@@ -3,17 +3,21 @@ import { SplitText } from "@utils/SplitText";
 import { AnimatePresence } from "framer-motion";
 
 export default function Header() {
-    const [visible, setVisible] = useState(false);
+    const [secondVisible, setSecondVisible] = useState(false);
+    const [thirdVisible, setThirdVisible] = useState(false);
     useEffect(async () => {
         setTimeout(() => {
-            setVisible(true);
+            setSecondVisible(true);
+            setTimeout(() => {
+                setThirdVisible(true);
+            }, 1000);
         }, 500);
     }, []);
     return (
         <div className="h-full flex justify-center items-center space-x-32 bg-background">
-            <div className="text-white">
+            <div className="text-white text-center">
                 <AnimatePresence>
-                    <div className="h-8 text-center -ml-2">
+                    <div className="mt-2 h-10 text-center -ml-2">
                         <SplitText
                             initial={{ y: "100%" }}
                             animate="visible"
@@ -32,8 +36,8 @@ export default function Header() {
                     </div>
                 </AnimatePresence>
                 <AnimatePresence>
-                    <div>
-                        {visible ? (
+                    <div className="h-15">
+                        {secondVisible ? (
                             <SplitText
                                 initial={{ y: "100%" }}
                                 animate="visible"
@@ -52,6 +56,31 @@ export default function Header() {
                         ) : (
                             <h1 className="text-7xl font-semibold invisible">
                                 I'm David Ilie
+                            </h1>
+                        )}
+                    </div>
+                </AnimatePresence>
+                <AnimatePresence>
+                    <div>
+                        {thirdVisible ? (
+                            <SplitText
+                                initial={{ y: "100%" }}
+                                animate="visible"
+                                className="mb-1 text-3xl font-semibold visible"
+                                variants={{
+                                    visible: (i) => ({
+                                        y: 0,
+                                        transition: {
+                                            delay: i * 0.25,
+                                        },
+                                    }),
+                                }}
+                            >
+                                a 14 year old Web Dev
+                            </SplitText>
+                        ) : (
+                            <h1 className="text-3xl font-semibold invisible">
+                                a 14 year old Web Dev
                             </h1>
                         )}
                     </div>
