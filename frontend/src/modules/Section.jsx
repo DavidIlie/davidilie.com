29eb6c10035e7987409d963f9d6e68ef8f878d25
David Ilie <david@davidilie.com>
2021-06-18 18:00:51 +0200

    started work on about me

--- frontend/src/modules/Section.jsx
+++ frontend/src/modules/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section(props) {
     return (
-        <div className="bg-white text-black pl-24 pr-24 pt-5 pb-5">
+        <div className="bg-white text-black pl-10 pr-10 lg:pl-24 lg:pr-24 pt-5 pb-5">
             <h1 className="text-5xl mb-5">{props.title || "Section"}</h1>
             {props.children}
         </div>
--- frontend/src/modules/landing/About.jsx
+++ frontend/src/modules/landing/About.jsx
@@ -3,15 +3,25 @@ import Section from "@modules/Section";
 export default function About() {
     return (
         <Section title="About Me">
-            <p className="text-1xl">
-                I'm David, a 14 year old software developer directing my focus
-                into web development. I've been interested into any kinds of
-                technology since I was little. I don't know how I got into it,
-                it could have been my father's career, or my discovery of the
-                huge world which is the internet, but nonetheless, I am proud
-                that I am pursuing a subject that can be used in as much ways as
-                possible to help others.
-            </p>
+            <div className="flex justify-center items-center space-x-10 flex-wrap md:flex-nowrap">
+                <img
+                    src="/images/me.png"
+                    className="rounded-full w-48 h-48 mb-2"
+                />
+                <p className="text-1xl mb-2" style={{ fontSize: "20px" }}>
+                    I'm David, a 14 year old software developer, focusing my
+                    learning into web development (full stack). I've been
+                    interested into computer science ever since I was small. I
+                    don't know what brought me into it, it could have been my
+                    father's career or my discovery of the huge world the
+                    internet has to offer. For the longest time I've been
+                    interested in the more technical aspects of running a
+                    website, not focusing much on the interface itself. The
+                    reason I created this website is to allow me to further
+                    experiment with both backend and frontend programming,
+                    allowing the world to see my progress.
+                </p>
+            </div>
         </Section>
     );
 }
