d5a1f68d273d0a5897ab1fe74d3e0e4b51a24643
David Ilie <david@davidilie.com>
2021-07-01 15:02:19 +0200

    agendaaaa

--- agenda/jobs/github.js
+++ agenda/jobs/github.js
@@ -12,48 +12,61 @@ const updateDavidIlieGitHubRepos = async (job, done) => {
 
     for (let i = 0; i < repos.length; i++) {
         const repo = await repos[i];
-        if (repo.name !== "DavidIlie") {
-            const dbRepo = await github.findOne({
+
+        const dbRepo = await github.findOne({
+            name: repo.name,
+        });
+
+        if (dbRepo) {
+            const updated = {
                 name: repo.name,
-            });
-            if (dbRepo) {
-                if (!_.isEqual(dbRepo, repo)) {
-                    await github.update(
-                        {
-                            name: repo.name,
-                        },
-                        {
-                            $set: {
-                                name: repo.name,
-                                url: repo.html_url,
-                                description: repo.description,
-                                language: repo.language,
-                                date: {
-                                    created_at: repo.created_at,
-                                    last_push: repo.pushed_at,
-                                },
-                                stars: repo.stargazers_count,
-                                issue_count: repo.open_issues,
-                            },
-                        }
-                    );
-                    updated = updated + 1;
-                }
-            } else {
-                await github.insert({
-                    name: repo.name,
-                    url: repo.html_url,
-                    description: repo.description,
-                    language: repo.language,
-                    date: {
-                        created_at: repo.created_at,
-                        last_push: repo.pushed_at,
+                url: repo.html_url,
+                description: repo.description,
+                language: repo.language,
+                date: {
+                    created_at: repo.created_at,
+                    last_push: repo.pushed_at,
+                },
+                stars: repo.stargazers_count,
+                issue_count: repo.open_issues,
+            };
+
+            if (!_.isEqual(repo, updated)) {
+                await github.update(
+                    {
+                        name: repo.name,
                     },
-                    stars: repo.stargazers_count,
-                    issue_count: repo.open_issues,
-                });
-                newRepos = newRepos + 1;
+                    {
+                        $set: {
+                            name: repo.name,
+                            url: repo.html_url,
+                            description: repo.description,
+                            language: repo.language,
+                            date: {
+                                created_at: repo.created_at,
+                                last_push: repo.pushed_at,
+                            },
+                            stars: repo.stargazers_count,
+                            issue_count: repo.open_issues,
+                        },
+                    }
+                );
+                updated = updated + 1;
             }
+        } else {
+            await github.insert({
+                name: repo.name,
+                url: repo.html_url,
+                description: repo.description,
+                language: repo.language,
+                date: {
+                    created_at: repo.created_at,
+                    last_push: repo.pushed_at,
+                },
+                stars: repo.stargazers_count,
+                issue_count: repo.open_issues,
+            });
+            newRepos = newRepos + 1;
         }
     }
 
