389f5cfee574d19fdf51cb1b0ecea93104d89330
David Ilie <david@davidilie.com>
2021-07-24 20:02:58 +0200

    made agendash redirect to api login if not authorized

--- agenda/utils/helpers/token.js
+++ agenda/utils/helpers/token.js
@@ -4,7 +4,7 @@ async function verifyToken(req, res, next) {
     if (req.originalUrl === "/api/agenda/dash/") {
         const token = req.cookies.access;
 
-        if (!token) return res.sendStatus(401);
+        if (!token) return res.redirect("https://davidilie.com/login");
 
         const validateRequest = await fetch(
             `${process.env.IDENTITY_SERVER_URL}/user`,
@@ -19,7 +19,7 @@ async function verifyToken(req, res, next) {
         if (validateRequest.status === 200) {
             next();
         } else {
-            res.sendStatus(401);
+            res.redirect("https://davidilie.com/login");
         }
     } else {
         next();
