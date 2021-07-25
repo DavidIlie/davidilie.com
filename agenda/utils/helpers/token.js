82d017b23b009c4212138108568a5dbd2e4ca794
David Ilie <david@davidilie.com>
2021-07-25 09:42:06 +0200

    started work on fallback

--- agenda/utils/helpers/token.js
+++ agenda/utils/helpers/token.js
@@ -4,7 +4,10 @@ async function verifyToken(req, res, next) {
     if (req.originalUrl === "/api/agenda/dash/") {
         const token = req.cookies.access;
 
-        if (!token) return res.redirect("https://davidilie.com/login");
+        if (!token)
+            return res.redirect(
+                "https://davidilie.com/login?fallback=https://davidilie.com/api/agenda/dash"
+            );
 
         const validateRequest = await fetch(
             `${process.env.IDENTITY_SERVER_URL}/user`,
@@ -19,7 +22,9 @@ async function verifyToken(req, res, next) {
         if (validateRequest.status === 200) {
             next();
         } else {
-            res.redirect("https://davidilie.com/login");
+            res.redirect(
+                "https://davidilie.com/login?fallback=https://davidilie.com/api/agenda/dash"
+            );
         }
     } else {
         next();
