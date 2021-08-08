7861069dd20a354cef7bf2f7a06cc713a7889c5b
David Ilie <david@davidilie.com>
2021-08-08 23:00:58 +0200

    removed useless react fragment

--- frontend/src/components/AdminPage/AdminPage.tsx
+++ frontend/src/components/AdminPage/AdminPage.tsx
@@ -14,19 +14,17 @@ const AdminPage = (props): JSX.Element => {
             {session?.user.isAdmin ? (
                 <div {...props}>{props.children}</div>
             ) : (
-                <>
-                    <Fade direction="down">
-                        <div className="flex flex-col justify-center items-center h-screen w-full">
-                            <h1 className="header-gradient font-semibold 2xl:text-6xl xl:text-6xl md:text-6xl lg:text-6xl text-4xl text-center">
-                                You are not an admin!
-                            </h1>
-                            <p className="text-gray-500 text-2xl">
-                                In order to see this page, you need
-                                administrator privileges.
-                            </p>
-                        </div>
-                    </Fade>
-                </>
+                <Fade direction="down">
+                    <div className="flex flex-col justify-center items-center h-screen w-full">
+                        <h1 className="header-gradient font-semibold 2xl:text-6xl xl:text-6xl md:text-6xl lg:text-6xl text-4xl text-center">
+                            You are not an admin!
+                        </h1>
+                        <p className="text-gray-500 text-2xl">
+                            In order to see this page, you need administrator
+                            privileges.
+                        </p>
+                    </div>
+                </Fade>
             )}
         </ProtectedPage>
     );
