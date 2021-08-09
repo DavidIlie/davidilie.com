e02cf4b2055d2979cec43b3207352687276dc644
David Ilie <david@davidilie.com>
2021-08-09 23:22:48 +0200

    fix

--- frontend/src/components/NavBar/UserDropdown.tsx
+++ frontend/src/components/NavBar/UserDropdown.tsx
@@ -18,93 +18,86 @@ const UserDropdown = (): JSX.Element => {
     if (loading) return null;
 
     return (
-        typeof window != "undefined" && (
-            <>
-                <Menu
-                    as="div"
-                    className="mt-1 relative inline-block text-right"
-                >
-                    <Menu.Button>
-                        {!session ? (
-                            <div className="transition duration-100 ease-in-out p-2 bg-gray-200 hover:bg-gray-300 rounded dark:bg-gray-800 dark:hover:bg-gray-700 cursor-pointer">
-                                <FiSettings
-                                    className="cursor-pointer rounded-full h-6 w-6 text-gray-500 dark:text-gray-400"
-                                    aria-label="Settings"
-                                    title="Settings"
-                                />
-                            </div>
-                        ) : (
-                            <Image
-                                width="40%"
-                                height="40%"
-                                className="cursor-pointer rounded-full"
-                                src={session.user.image}
-                                blurDataURL={
-                                    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAIklEQVQImWNgYBDnFZCxsfc0tvJkCI/O+vP/f0tdhZK8CgBLPAfARKUieAAAAABJRU5ErkJggg=="
-                                }
-                                placeholder="blur"
-                                aria-label="Settings+Account"
-                                title="Settings+Account"
+        <>
+            <Menu as="div" className="mt-1 relative inline-block text-right">
+                <Menu.Button>
+                    {!session ? (
+                        <div className="transition duration-100 ease-in-out p-2 bg-gray-200 hover:bg-gray-300 rounded dark:bg-gray-800 dark:hover:bg-gray-700 cursor-pointer">
+                            <FiSettings
+                                className="cursor-pointer rounded-full h-6 w-6 text-gray-500 dark:text-gray-400"
+                                aria-label="Settings"
+                                title="Settings"
                             />
-                        )}
-                    </Menu.Button>
-                    <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                    >
-                        <Menu.Items className="absolute right-0 w-36 bg-gray-100 dark:bg-gray-800 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <div className="border-1 rounded-t-md border-gray-200 dark:border-gray-900">
-                                <Menu.Item>
-                                    <a
-                                        href="https://github.com/DavidIlie/davidilie.com/issues"
-                                        target="_blank"
-                                        aria-label="Submit a bug report"
-                                        title="Submit a bug report"
-                                    >
-                                        <DropdownElement>
-                                            <AiFillBug className="text-2xl" />
-                                            Report a bug
-                                        </DropdownElement>
-                                    </a>
-                                </Menu.Item>
-                                <Menu.Item as={DropdownElement}>
-                                    <ThemeToggle />
-                                </Menu.Item>
-                            </div>
+                        </div>
+                    ) : (
+                        <Image
+                            width="40%"
+                            height="40%"
+                            className="cursor-pointer rounded-full"
+                            src={session.user.image}
+                            blurDataURL={
+                                "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAIklEQVQImWNgYBDnFZCxsfc0tvJkCI/O+vP/f0tdhZK8CgBLPAfARKUieAAAAABJRU5ErkJggg=="
+                            }
+                            placeholder="blur"
+                            aria-label="Settings+Account"
+                            title="Settings+Account"
+                        />
+                    )}
+                </Menu.Button>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className="absolute right-0 w-36 bg-gray-100 dark:bg-gray-800 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="border-1 rounded-t-md border-gray-200 dark:border-gray-900">
                             <Menu.Item>
                                 <a
-                                    className="font-semibold cursor-pointer bg-gray-300 dark:bg-blue-800 text-center group flex justify-center rounded-b-md items-center w-full py-2 text-sm"
-                                    onClick={() =>
-                                        !session
-                                            ? setModalOpen(true)
-                                            : signOut()
-                                    }
-                                    aria-label={`Click to log ${
-                                        !session ? "in" : "out"
-                                    } to your account`}
-                                    title={`Click to log ${
-                                        !session ? "in" : "out"
-                                    } to your account`}
+                                    href="https://github.com/DavidIlie/davidilie.com/issues"
+                                    target="_blank"
+                                    aria-label="Submit a bug report"
+                                    title="Submit a bug report"
                                 >
-                                    {!session ? "Log In" : "Log Out"}
+                                    <DropdownElement>
+                                        <AiFillBug className="text-2xl" />
+                                        Report a bug
+                                    </DropdownElement>
                                 </a>
                             </Menu.Item>
-                        </Menu.Items>
-                    </Transition>
-                </Menu>
-                {modalOpen ? (
-                    <LoginModal
-                        isOpen={modalOpen}
-                        onClose={() => setModalOpen(false)}
-                    />
-                ) : null}
-            </>
-        )
+                            <Menu.Item as={DropdownElement}>
+                                <ThemeToggle />
+                            </Menu.Item>
+                        </div>
+                        <Menu.Item>
+                            <a
+                                className="font-semibold cursor-pointer bg-gray-300 dark:bg-blue-800 text-center group flex justify-center rounded-b-md items-center w-full py-2 text-sm"
+                                onClick={() =>
+                                    !session ? setModalOpen(true) : signOut()
+                                }
+                                aria-label={`Click to log ${
+                                    !session ? "in" : "out"
+                                } to your account`}
+                                title={`Click to log ${
+                                    !session ? "in" : "out"
+                                } to your account`}
+                            >
+                                {!session ? "Log In" : "Log Out"}
+                            </a>
+                        </Menu.Item>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
+            {modalOpen ? (
+                <LoginModal
+                    isOpen={modalOpen}
+                    onClose={() => setModalOpen(false)}
+                />
+            ) : null}
+        </>
     );
 };
 
