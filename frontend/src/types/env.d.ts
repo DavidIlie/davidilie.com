6d475145e9de4a758b030de86d0dd8ad2876e7e6
David Alexandru Ilie <david@davidilie.com>
2021-08-13 15:35:37 +0200

    added env types

---
+++ frontend/src/types/env.d.ts
@@ -0,0 +1,13 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_HOST: string;
+    MONGO_URI: string;
+    GOOGLE_CLIENT_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    DISCORD_SECRET: string;
+    DISCORD_CLIENT_ID: string;
+    NEXTAUTH_URL: string;
+    IDENTITY_SERVER_URL: string;
+    JWT_SECRET: string;
+  }
+}
