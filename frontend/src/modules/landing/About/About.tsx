7f272afd750776ea86d308063b1cab741f911a1d
David Ilie <david@davidilie.com>
2022-02-28 15:31:19 +0100

    (frontend): updated about me

--- frontend/src/modules/landing/About/About.tsx
+++ frontend/src/modules/landing/About/About.tsx
@@ -7,7 +7,7 @@ import { ContentCreator } from "./skills/ContentCreator";
 export const About = () => {
     return (
         <>
-            <div className="bg-blue-600 dark:bg-blue-800 text-white dark:text-gray-100 2xl:text-center xl:text-center md:text-center text-left px-10 pt-12 pb-32">
+            <div className="px-10 pt-12 pb-32 text-left text-white bg-blue-600 dark:bg-blue-800 dark:text-gray-100 2xl:text-center xl:text-center md:text-center">
                 <Fade direction="left" triggerOnce cascade>
                     <h1
                         className={`text-center 2xl:text-5xl xl:text-5xl md:text-5xl lg:text-4xl text-4xl font-semibold mb-5`}
@@ -17,13 +17,12 @@ export const About = () => {
                     <h1
                         className={`text-section 2xl:px-96 xl:px-32 lg:px-24 md:px-24 sm:px-5 mb-32`}
                     >
-                        I&apos;m a 15 year old software developer, focusing my
-                        learning into web development. I&apos;ve been interested
-                        into computer science ever since I was younger but never
-                        got into proper development until about 2 years ago. I
-                        don&apos;t know what brought me into it, it could have
-                        been my father&apos;s career or my discovery of the huge
-                        world the internet has to offer. For a while i&apos;ve
+                        I&apos;m a 15 year old software developer and full-time
+                        student. I&apos;ve been interested into computer science
+                        ever since I was younger but never got into proper
+                        development until about 2 years ago, it's always kept me
+                        going and motivated throughout the years which is why I
+                        want to pursue it in the future. For a while i&apos;ve
                         found myself looking into how a website functions, but I
                         never tried doing it myself until recently. The goal of
                         this website is to express my{" "}
