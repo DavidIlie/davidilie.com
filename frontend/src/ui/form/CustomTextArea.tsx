a8382605abedf22e0cd9aff634043088ab8270c3
David Ilie <david@davidilie.com>
2022-03-08 17:25:36 +0100

    (frontend): fix contact page

--- frontend/src/ui/form/CustomTextArea.tsx
+++ frontend/src/ui/form/CustomTextArea.tsx
@@ -2,7 +2,7 @@ const CustomTextArea = (props) => {
     return (
         <textarea
             {...props}
-            className="relative w-full px-4 py-3 pr-10 mt-3 text-sm placeholder-gray-600 bg-gray-300 border-0 shadow outline-none dark:placeholder-gray-300 dark:bg-gray-900 rounded-xl focus:outline-none focus:ring"
+            className="w-full px-4 py-3 pr-10 mt-3 text-sm placeholder-gray-600 bg-gray-300 border-0 shadow outline-none dark:placeholder-gray-300 dark:bg-gray-900 rounded-xl focus:outline-none focus:ring"
         />
     );
 };
