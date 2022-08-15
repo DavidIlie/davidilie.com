49b59551a90db7ddc0c24c08d08b4a58f8528b56
David Ilie <david@davidilie.com>
2022-08-15 14:00:50 +0300

    test new dockerfile

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
          "i.scdn.co",
       ],
    },
+   experimental: {
+      outputStandalone: true,
+      concurrentFeatures: true,
+   },
 };
 
 module.exports = withPlugins([withPlausibleProxy, withMDX, nextConfig]);
