d0216b58c3108fb400482e9497d80a7fa52c144c
David Ilie <david@davidilie.com>
2022-08-31 21:14:47 +0100

    work on the projects page

---
+++ src/data/projects.ts
@@ -0,0 +1,27 @@
+interface ProjectGitHub {
+   repo_id: string;
+   name: string;
+   description: string;
+   image: string;
+}
+
+interface ProjectNoGitHub {
+   image: string;
+   name: string;
+   description: string;
+   language: string;
+}
+
+type Project = ProjectNoGitHub | ProjectGitHub;
+
+const projects: Project[] = [
+   {
+      repo_id: "davidilie.com",
+      name: "Personal Website",
+      description:
+         "I've always seen my website as a place where I can experiment with the different options that are available with Web Development. This website currently uses the T3 Stack, and from the enjoyment I've had coding this website, I will use this stack in the future.",
+      image: "https://user-images.githubusercontent.com/47594764/187702010-acb78b71-b0f2-4c33-a11b-d5ac4c78d77d.png",
+   },
+];
+
+export default projects;
---
+++ src/pages/projects.tsx
@@ -0,0 +1,56 @@
+import { NextPage } from "next";
+import { NextSeo } from "next-seo";
+import { Fade } from "react-awesome-reveal";
+
+import { trpc } from "@/lib/trpc";
+import projects from "@/data/projects";
+
+import SmallProject from "@/components/ProjectComponents/SmallProject";
+
+const Projects: NextPage = () => {
+   const { data } = trpc.useQuery(["getProjects"]);
+
+   return (
+      <>
+         <NextSeo title="Projects" />
+         <div className="flex justify-center flex-grow mt-32 mb-12 sm:px-6 lg:px-8">
+            <div className="text-center">
+               <Fade direction="up" triggerOnce cascade>
+                  <h1 className="pb-2 text-4xl font-bold text-center sm:text-6xl header-gradient">
+                     Projects
+                  </h1>
+               </Fade>
+               <h1 className="pb-2 text-4xl font-bold text-center sm:text-6xl header-gradient">
+                  Repositories
+               </h1>
+               <p className="mt-2 mb-4 text-section">
+                  A list of all my public repositories on{" "}
+                  <a
+                     href="https://github.com/davidilie"
+                     target="_blank"
+                     rel="noreferrer"
+                     className="duration-150 hover:text-blue-500"
+                  >
+                     GitHub
+                  </a>
+                  .
+               </p>
+               <div className="container grid max-w-6xl grid-cols-3 gap-4 mx-auto">
+                  {data
+                     ?.sort(
+                        (a, b) =>
+                           new Date(a.lastPush).getTime() -
+                           new Date(b.lastPush).getTime()
+                     )
+                     .reverse()
+                     .map((project) => (
+                        <SmallProject project={project} key={project.name} />
+                     ))}
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
+
+export default Projects;
