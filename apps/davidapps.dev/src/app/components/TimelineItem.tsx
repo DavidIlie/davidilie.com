ddee64f64708c179f655505da7316d68de6276e6
David Ilie <david@davidilie.com>
2023-06-07 20:50:38 +0100

    i am sorry

--- apps/davidapps.dev/src/app/components/TimelineItem.tsx
+++ apps/davidapps.dev/src/app/components/TimelineItem.tsx
@@ -2,38 +2,43 @@ import React from "react";
 import Image from "next/image";
 
 import { AiOutlineLink } from "react-icons/ai";
+import { IconType } from "react-icons";
 
-export interface TimelineItemProps {
+export type TimelineItemProps = {
    title: string;
    description: React.ReactNode;
    link?: string;
-   imageSrc: string;
+   image: string | IconType;
    small?: boolean;
-}
+};
 
 const TimelineItem: React.FC<TimelineItemProps> = ({
    title,
    description,
    link,
-   imageSrc,
    small = false,
+   ...rest
 }) => {
    const widthStyle = small ? "w-24 h-24" : "w-64 w-64";
 
    return (
-      <div className="relative flex items-center gap-12">
+      <div className="relative flex gap-12">
          <div className="absolute w-1 h-full bg-gray-300 left-[7.8rem]"></div>
          <div className={`relative z-50 flex-shrink-0 ${small && "ml-20"}`}>
             <div
                className={`flex items-center justify-center ${widthStyle} bg-gray-600 p-1 rounded-full`}
             >
-               <Image
-                  src={imageSrc}
-                  alt={title}
-                  className={`object-scale-down ${widthStyle} rounded-full`}
-                  width={small ? 24 * 6 : 64 * 6}
-                  height={small ? 24 * 6 : 64 * 6}
-               />
+               {typeof rest.image !== "string" ? (
+                  <rest.image className="text-6xl" />
+               ) : (
+                  <Image
+                     src={rest.image}
+                     alt={title}
+                     className={`${widthStyle} object-cover rounded-full`}
+                     width={small ? 24 * 6 : 64 * 6}
+                     height={small ? 24 * 6 : 64 * 6}
+                  />
+               )}
             </div>
          </div>
          <div className="px-4 py-2 bg-gray-800 border-2 border-gray-700 rounded-md">
@@ -55,7 +60,7 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
                   </a>
                )}
             </div>
-            <p className="text-gray-300">{description}</p>
+            <p className="text-gray-200">{description}</p>
          </div>
       </div>
    );
--- apps/davidapps.dev/src/data/timeline.tsx
+++ apps/davidapps.dev/src/data/timeline.tsx
@@ -1,6 +1,9 @@
 import { ExternalLink } from "ui";
 import { TimelineItemProps } from "~/app/components/TimelineItem";
 
+import { FaServer } from "react-icons/fa";
+import { SiProxmox } from "react-icons/si";
+
 export const items: TimelineItemProps[] = [
    {
       title: "Servers",
@@ -23,35 +26,61 @@ export const items: TimelineItemProps[] = [
             handed down HP Gen 6 Server.
          </p>
       ),
-      imageSrc: "/servers.webp",
+      image: FaServer,
+      small: true,
    },
    {
       title: "Nucleus",
       description: (
-         <p>
-            My current server is a Dell R730, named Nucleus, with two
-            <ExternalLink url="https://www.intel.co.uk/content/www/uk/en/products/sku/92981/intel-xeon-processor-e52630-v4-25m-cache-2-20-ghz/specifications.html">
-               Intel E5-2630 v4
-            </ExternalLink>{" "}
-            CPUs, 224GB ECC DDR4 RAM, 8 500GB Sata SSDs via Dell's PERC HBA (4
-            in ZFS for VMs, 4 for Kubernetes), an{" "}
-            <ExternalLink url="https://www.amd.com/en/support/graphics/radeon-500-series/radeon-rx-500-series/radeon-rx-580">
-               AMD RX 580
-            </ExternalLink>{" "}
-            for Plex and Remote Gaming and two 500GB NVME SSDS from Intel and
-            Samsung for more VMs. 1x4TB 3x1TB HHDs on a Gen 6 HP Server
-            backplane through a HP P420i HBA controller. That's a lot of
-            overkill hardware but my server is composed of
+         <div>
+            <p>My current server is a Dell R730, named Nucleus:</p>
+            <ul className="mx-4 list-disc marker:text-blue-500">
+               <li>
+                  Two
+                  <ExternalLink url="https://www.intel.co.uk/content/www/uk/en/products/sku/92981/intel-xeon-processor-e52630-v4-25m-cache-2-20-ghz/specifications.html">
+                     Intel E5-2630 v4
+                  </ExternalLink>{" "}
+                  CPUs
+               </li>
+               <li>224GB ECC DDR4 RAM</li>
+               <li>2x500GB Sata SSDs in ZFS Raid 1 for OS</li>
+               <li>
+                  8x500GB Sata SSDs via Dell's PERC HBA (4 in ZFS for VMs, 4 for
+                  Kubernetes)
+               </li>
+               <li>
+                  <ExternalLink url="https://www.amd.com/en/support/graphics/radeon-500-series/radeon-rx-500-series/radeon-rx-580">
+                     AMD RX 580
+                  </ExternalLink>{" "}
+                  for Plex and Remote Gaming
+               </li>
+               <li>2x500GB NVME SSDS from Intel and Samsung for more VMs</li>
+               <li>
+                  1x4TB 3x1TB HHDs on a Gen 6 HP Server backplane through a HP
+                  P420i HBA controller.
+               </li>
+            </ul>
+            That's a lot of overkill hardware but my server is composed of
             hardware/servers/computers that I've had laying around and it was
             better to put it all in one.
-         </p>
+         </div>
       ),
-      imageSrc: "/server.gif",
+      image: "/server.gif",
    },
    {
       title: "Hypervisor",
-      description: "Proxmox has been ",
-      imageSrc: "/proxmox.png",
+      description: (
+         <p>
+            Proxmox has been my choice as the main host operating system and
+            hypervisor for the sole reason of <strong>reliability</strong>. I
+            know it can withstand long amounts of uptime and has the best VM
+            performance. I used to use its LXC container feature but not
+            anymore, you'll see why as you continue reading. I am using a RAID 1
+            for my bootdrive as the only problems I've had are with the drives
+            themselves so I want ultimate realibility while I am away.
+         </p>
+      ),
+      image: SiProxmox,
       link: "https://www.proxmox.com/en/",
       small: true,
    },
