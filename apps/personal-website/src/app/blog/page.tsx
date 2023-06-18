2f20d98715ed28657c84ab838a369be325beb0de
David Ilie <david@davidilie.com>
2023-06-18 20:52:24 +0100

    fantastic

--- apps/personal-website/src/app/blog/page.tsx
+++ apps/personal-website/src/app/blog/page.tsx
@@ -11,22 +11,20 @@ const Page = () => {
    const featuredPost = allBlogs[0] as Blog;
    return (
       <div className="flex flex-grow items-center justify-center sm:px-6 lg:px-8">
-         <div className="container mx-auto mt-32 mb-12 max-w-4xl">
+         <div className="container mx-auto mb-12 mt-32 max-w-4xl">
             <h1 className="gradient-text text-center text-5xl font-bold">
                The David Ones
             </h1>
-            <p className="mt-4 mb-6 px-6 text-center sm:mb-0 md:px-12 md:text-lg">
+            <p className="mb-6 mt-4 px-6 text-center sm:mb-0 md:px-12 md:text-lg">
                I&apos;ve been writing blog posts since 2020, mostly about my
                random technologic encounters during my day-to-day life.
                Currently there are {allBlogs.length} blog post
                {allBlogs.length > 1 && "s"}.
             </p>
-            <div className="px-4 sm:px-1">
-               {/*
-                  // @ts-expect-error Server Component*/}
-               <PostCard {...featuredPost} featured />
-            </div>
-            <div className="mt-5 grid grid-cols-1 gap-2 px-4 sm:grid-cols-2">
+            {/*
+               // @ts-expect-error Server Component*/}
+            <PostCard {...featuredPost} featured />
+            <div className="mt-5 grid grid-cols-1 gap-2 sm:grid-cols-2 sm:px-2.5">
                {allBlogs
                   .sort((a, b) => {
                      if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
