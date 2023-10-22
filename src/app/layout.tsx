7982dd5d80c508193e66ea1d6fde5d39b483f170
David Ilie <david@davidilie.com>
2023-10-22 10:28:03 +0200

    chore: change some stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
    children: React.ReactNode;
 }) {
    return (
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
          <body className={`text-black dark:text-white ${graphik.variable}`}>
             <Providers>
                <BackgroundPattern>
