0ce2347d3986e60bb839b2b0839000f69c40ec11
David Alexandru Ilie <david@davidilie.com>
2024-10-25 17:00:17 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ const graphik = localFont({
 });
 
 const SEO_DESCRIPTION =
-   "A 17 year old aspiring web developer experimenting with programming by publishing my work on the web for others to see and use.";
+   "An 18 year old aspiring web developer experimenting with programming by publishing my work on the web for others to see and use.";
 
 export const viewport: Viewport = {
    colorScheme: "dark light",
